e43bdae9e633d0ceb89e24c02decf2483a076b50
Ricardo Quesada <ricardoquesada@gmail.com>
2012-09-05 18:07:49 -0700

    compiles on iOS + ARM

--- js/src/vm/NumericConversions.h
+++ js/src/vm/NumericConversions.h
@@ -110,7 +110,7 @@ ToInt32(double d)
     }
 
     return int32_t(du.d);
-#elif defined (__arm__) && defined (__GNUC__)
+#elif defined (__arm__) && defined (__GNUC__) && defined(IGNORE_BECAUSE_IT_DOES_NOT_COMPILE)
     int32_t i;
     uint32_t    tmp0;
     uint32_t    tmp1;
