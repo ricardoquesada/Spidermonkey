807895d356a27db037b7eefb06ca08e223366ac4
James Chen <jianhua.chen@cocos2d-x.org>
2013-05-22 18:13:21 +0800

    Enable JIT support for win32.

--- js/src/build-win32/build.sh
+++ js/src/build-win32/build.sh
@@ -2,10 +2,7 @@
 
 # configure
 ../configure --disable-tests \
-             --disable-debug \
-             --disable-ion \
-             --disable-jm \
-             --disable-tm
+             --disable-debug 
 
 # make
 make -j4
