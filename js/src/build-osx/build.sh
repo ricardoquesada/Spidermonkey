b9150f7cd5aba2f91cbaf1ae6c145234878bac75
pandamicro <lphuabin@gmail.com>
2016-02-14 14:56:27 +0800

    Update build scripts

--- js/src/build-osx/build.sh
+++ js/src/build-osx/build.sh
@@ -5,17 +5,21 @@ cpus=$(sysctl hw.ncpu | awk '{print $2}')
 # configure
 ../configure --disable-tests --disable-shared-js \
             --enable-strip --enable-strip-install \
-            --disable-root-analysis --disable-exact-rooting --enable-gcincremental --enable-optimize=-O3 \
+            --disable-gcgenerational --disable-exact-rooting \
+            --disable-root-analysis --enable-gcincremental --enable-optimize=-O3 \
             --enable-llvm-hacks \
             --disable-debug \
+            --disable-gczeal \
             --without-intl-api \
             --disable-threadsafe
 # make
 xcrun make -j$cpus
 
 # strip
-xcrun strip -S libjs_static.a
+xcrun strip -S js/src/libjs_static.a
 
 # info
-xcrun lipo -info libjs_static.a
+xcrun lipo -info js/src/libjs_static.a
+
+mv js/src/libjs_static.a libjs_static.a
 
--- js/src/build-win32/build.sh
+++ js/src/build-win32/build.sh
@@ -2,9 +2,13 @@
 
 # configure
 ../configure --disable-tests \
+             --enable-strip --enable-strip-install \
+             --disable-gcgenerational --disable-exact-rooting \
+             --disable-root-analysis --enable-gcincremental \
              --disable-debug \
+             --disable-gczeal \
              --without-intl-api \
              --disable-threadsafe
 
 # make
-../../../build/pymake/make.py -j4
+mozmake -j8
